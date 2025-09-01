d8906a016e783e8c4bb9422202c46c99cd7870b1
bahaeddine.chouari <BahaEddine.Chouari@Billcom-Consulting.com>
2025-09-01 10:16:23 +0100

    x

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{astro,js,ts,jsx,tsx,vue,html}",
-  ],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)", "system-ui", "sans-serif"],
-        serif: ["var(--font-serif)", "ui-serif", "serif"],
-      },
-      colors: {
-        primary: "var(--color-primary)",
-        neutral: "var(--color-neutral)",
-        white: "var(--color-white)",
-        black: "var(--color-black)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
